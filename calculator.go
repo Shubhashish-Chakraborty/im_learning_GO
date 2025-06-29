42e7f7c0a22f9fe2efb18f989cf9742ce5ae351e
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-06-29 22:34:59 +0530

    implemeted switch statements to the conditional logic

--- calculator.go
+++ calculator.go
@@ -6,39 +6,40 @@ import (
 
 func main() {
 	// basic Calculator written in Go!!
-	var firstnumber int;
-	var operation string;
-	var secondNumber int;
-	var askExit string;
+	var firstnumber int
+	var operation string
+	var secondNumber int
+	var askExit string
 
 	for {
-		fmt.Print("\n");
-		fmt.Print("Enter First Number: ");
-		fmt.Scan(&firstnumber);
-		
-		fmt.Print("Operation(+ , - , / , *):  ");
-		fmt.Scan(&operation);
-		
-		fmt.Print("Enter Second Number: ");
-		fmt.Scan(&secondNumber);
+		fmt.Print("\n")
+		fmt.Print("Enter First Number: ")
+		fmt.Scan(&firstnumber)
 
-		if (operation == "+") {
-			fmt.Printf("%v %v %v = %v \n\n" , firstnumber, operation, secondNumber, firstnumber + secondNumber);
-		} else if (operation == "-") {
-			fmt.Printf("%v %v %v = %v \n\n" , firstnumber, operation, secondNumber, firstnumber - secondNumber);
-		} else if (operation == "/") {
-			fmt.Printf("%v %v %v = %v \n\n" , firstnumber, operation, secondNumber, firstnumber / secondNumber);
-		} else if (operation == "*") {
-			fmt.Printf("%v %v %v = %v \n\n" , firstnumber, operation, secondNumber, firstnumber * secondNumber);
-		} else {
-			fmt.Printf("Something Went Wrong, Please Try Again! \n\n");
+		fmt.Print("Operation(+ , - , / , *):  ")
+		fmt.Scan(&operation)
+
+		fmt.Print("Enter Second Number: ")
+		fmt.Scan(&secondNumber)
+
+		switch operation {
+		case "+":
+			fmt.Printf("%v %v %v = %v \n\n", firstnumber, operation, secondNumber, firstnumber+secondNumber)
+		case "-":
+			fmt.Printf("%v %v %v = %v \n\n", firstnumber, operation, secondNumber, firstnumber-secondNumber)
+		case "*":
+			fmt.Printf("%v %v %v = %v \n\n", firstnumber, operation, secondNumber, firstnumber*secondNumber)
+		case "/":
+			fmt.Printf("%v %v %v = %v \n\n", firstnumber, operation, secondNumber, firstnumber/secondNumber)
+		default:
+			fmt.Printf("Something Went Wrong, Please Try Again! \n\n")
 		}
-		
-		fmt.Print("Exit??(y/n):");
-		fmt.Scan(&askExit);
 
-		if (askExit == "y") {
-			break;
+		fmt.Print("Exit??(y/n):")
+		fmt.Scan(&askExit)
+
+		if askExit == "y" {
+			break
 		}
 	}
 }
