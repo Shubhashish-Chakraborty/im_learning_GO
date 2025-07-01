e38cf03b190f72bf5d5c859a30d18358f50d9d3d
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-07-02 01:04:59 +0530

    / endpoint handled, with multiple application types

--- kiddy-projects/dummyapi/main.go
+++ kiddy-projects/dummyapi/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"log"
 	"dummyapi/handlers"
+	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -12,8 +13,29 @@ func main() {
 	PORT := "3000";
 	r := httprouter.New();
 
+	// // use this if returning json on any end point 
+	// type Response struct {
+	// 	Message string `json:"message"`
+	// }
+
 	// Routes Handling:
-	
+
+	r.GET("/" , func (w http.ResponseWriter , r * http.Request , _ httprouter.Params) {
+		// w.Header().Set("Content-Type" , "application/json");
+		// json.NewEncoder(w).Encode(Response{Message: "Server is Up"})
+
+		// so either pass json or simple plain text:
+
+		// w.Header().Set("Content-Type" , "text/plain");
+		// fmt.Fprintln(w , "HELLO THE SERVER IS UP");
+
+		// or a web page (html) :
+
+		w.Header().Set("Content-Type" , "text/html");
+		fmt.Fprintf(w , "Server is Up!")
+		
+	})
+
 	r.GET("/jokes" , handlers.GetAllJokes);
 	r.GET("/jokes/random" , handlers.GetRandomJoke);
 	
